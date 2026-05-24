4e12a94a1a32d415f82ab305d0929ddea30a0bf9
NadirYusifov <nadiryusifov448@gmail.com>
2026-05-25 02:49:37 +0400

    update: text remove, added ListCollapse icon in Lucide Icon and text changed style

--- src/layouts/otherLinks/otherLinks.tsx
+++ src/layouts/otherLinks/otherLinks.tsx
@@ -7,6 +7,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { ListCollapse } from "lucide-react";
 
 export const OtherLink = () => {
   return (
@@ -15,25 +16,25 @@ export const OtherLink = () => {
         render={
           <Button
             size="lg"
-            className="transtion-all text-md fixed right-4 bottom-4 h-12 w-12 cursor-pointer bg-olive-500 duration-200 ease-in-out hover:-translate-y-1 hover:shadow-lg"
-          />
+            className="transtion-all text-md border-border hover:bg-muted fixed right-4 bottom-4 h-12 w-12 cursor-pointer border bg-white text-neutral-800 duration-200 ease-in-out hover:-translate-y-1 hover:shadow-lg"
+          >
+            <ListCollapse />
+          </Button>
         }
-      >
-        Aç
-      </DropdownMenuTrigger>
+      ></DropdownMenuTrigger>
       <DropdownMenuContent className="w-40">
         <DropdownMenuGroup>
           <DropdownMenuLabel className="text-md">
             Digər keçidlər
           </DropdownMenuLabel>
           <DropdownMenuItem
-            className="text-md cursor-pointer"
+            className="text-md cursor-pointer text-neutral-800"
             render={<a href="#projects" />}
           >
             Layihələr
           </DropdownMenuItem>
           <DropdownMenuItem
-            className="text-md cursor-pointer"
+            className="text-md cursor-pointer text-neutral-800"
             render={<a href="#experience" />}
           >
             İş təcrübəm
