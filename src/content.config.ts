0c0a0db08748d44ffffdf3cd28e336b2d9ce53e5
NadirYusifov <nadiryusifov448@gmail.com>
2026-06-04 01:04:24 +0400

    update: import file col changed and defineCollection from projects and experincesWork inside nested `.json` documents from Asrto docs and schema update and added experiencesWork defineCollection object inside loader nad schema with Zod

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,42 @@
-import { file } from "astro/loaders";
 import { z } from "astro/zod";
+import { file } from "astro/loaders";
 import { defineCollection } from "astro:content";
 
-const projects = defineCollection({
-  loader: file("src/data/projects.json"),
+const projectsOwn = defineCollection({
+  // Nested `.json` documents -- Astro
+  loader: file("src/data/projects.json", {
+    parser: (project) => JSON.parse(project).projects,
+  }),
   schema: z.object({
-    id: z.string(),
+    id: z.number(),
     title: z.string(),
     description: z.string(),
-    links: z.object({
+    url: z.object({
       github: z.optional(z.string()),
       demo: z.optional(z.string()),
     }),
   }),
 });
 
-export const collections = { projects };
+const experincesWork = defineCollection({
+  loader: file("src/data/experiences.json", {
+    parser: (experince) => JSON.parse(experince).experiences,
+  }),
+  schema: z.object({
+    id: z.number(),
+    title: z.string(),
+    description: z.string(),
+    date: z.object({
+      startdate: z.string(),
+      choice: z.object({
+        discriminant: z.string(),
+        value: z.string(),
+      }),
+    }),
+    url: z.object({
+      demo: z.optional(z.url())
+    })
+  }),
+});
+
+export const collections = { projectsOwn, experincesWork };
