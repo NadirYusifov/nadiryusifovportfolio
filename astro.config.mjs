ead7a64b5032485faf52bf834ea8fd48535b18fb
NadirYusifov <nadiryusifov448@gmail.com>
2026-06-18 17:11:17 +0400

    update: added ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,6 @@ export default defineConfig({
     },
   },
 
-  integrations: [react(), (process.env.SKIP_KEYSTATIC ? [] : [keystatic()]), mdx()],
+  integrations: [react(), ...(process.env.SKIP_KEYSTATIC ? [] : [keystatic()]), mdx()],
   adapter: netlify(),
 });
